a9bdd538faf162752f6e897bd81a64fa5b606594
Vinit Raut <vinit.coder@gmail.com>
2022-02-25 23:55:32 +0530

    Connect MongoDB with app

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import connectDB from './db/connect.js';
 
 dotenv.config();
 const app = express();
@@ -8,12 +9,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-const start = () => {
-  app.listen(PORT, () => {
-    console.log(
-      `Server listening in ${NODE_ENV} mode on port ${PORT}`.yellow.bold
-    );
-  });
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log(`MongoDB Connected`.cyan.underline.bold);
+
+    app.listen(PORT, () => {
+      console.log(
+        `Server listening in ${NODE_ENV} mode on port ${PORT}`.yellow.bold
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
