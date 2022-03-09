561f0472ef580fdc3f6611eeed05765b2b770301
Vinit Raut <vinit.coder@gmail.com>
2022-03-10 01:07:57 +0530

    Add: Profile model

---
+++ backend/models/profileModel.js
@@ -0,0 +1,85 @@
+import mongoose from 'mongoose';
+
+const ProfileSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    company: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: [String],
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    githubusername: {
+      type: String,
+    },
+    experience: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        company: {
+          type: String,
+          required: true,
+        },
+        location: {
+          type: String,
+        },
+        from: {
+          type: Date,
+          required: true,
+        },
+        to: {
+          type: Date,
+        },
+        current: {
+          type: Boolean,
+          default: false,
+        },
+        description: {
+          type: String,
+        },
+      },
+    ],
+    social: {
+      twitter: {
+        type: String,
+      },
+      linkedin: {
+        type: String,
+      },
+      github: {
+        type: String,
+      },
+      peerlist: {
+        type: String,
+      },
+      blog: {
+        type: String,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Profile = mongoose.model('Profile', ProfileSchema);
+export default Profile;
